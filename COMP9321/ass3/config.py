5fd519014ec4208dd22a42ccfc49d8098b7df7c2
wuzeshixp <159595223@qq.com>
2018-05-17 20:54:44 +1000

    simple user model and base page

---
+++ COMP9321/ass3/config.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'COMP9321assn3'
+DEBUG = True
+
+MONGODB_DB = '9321assn3'
+MONGODB_HOST = 'ds123500.mlab.com'
+MONGODB_PORT = 23500
+MONGODB_USERNAME = '9321assn3Admin'
+MONGODB_PASSWORD = '9321assn3Admin'
+
+
+#mongodb://<dbuser>:<dbpassword>@ds123500.mlab.com:23500/9321assn3
---
+++ COMP9321/ass3/user/view.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template
+from user.models import User
+
+user_page = Blueprint('user_page',__name__)
+
+@user_page.route('/login')
+def login():
+
+    # user = User(name = 'zeshi', password='123',email= 'email@gmail.com')
+    # user.save
+    #return "HI,{}!, Your email is {}".format(user.name,user.email)
+    return render_template('base.html')
+
+@user_page.route('/signup')
+def signup():
+    return render_template('user/signup.html')
