672e34f41487ad09aafe9a97cf06674a867da2f2
SURFACE-Y\R&Y <minzhenye@126.com>
2018-05-18 14:01:52 +1000

    store busInfo into mongodb

--- COMP9321/ass3/server/views.py
+++ COMP9321/ass3/server/views.py
@@ -12,40 +12,48 @@ app.config["MONGO_DBNAME"] = "ass3db"
 app.config["MONGO_URI"] = "mongodb://admin:admin@ds119650.mlab.com:19650/ass3db"
 mongo = PyMongo(app)
 
-# Insert the dictionary into Mongo
-@app.route('/',methods=['GET'])
-def storeCsv():
+#Store information about australia using meetup api
+@app.route('/meetup/',methods=['GET'])
+def storeMeetup():
+    storeToMongodb("https://api.meetup.com/2/cities?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721","cities")
+    storeToMongodb("https://api.meetup.com/2/categories?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721","categories")
+    storeToMongodb("https://api.meetup.com/2/groups?sign=true&photo-host=public&country=au&key=37357c27353310f2d4f417f855721&city=Sydney","groups")
+    storeToMongodb("https://api.meetup.com/find/upcoming_events?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721&lon=151.2100067138672&lat=-33.869998931884766","events")
 
-        getResponseCode("https://api.meetup.com/2/cities?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721","cities")
-        getResponseCode("https://api.meetup.com/2/categories?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721","categories")
-        getResponseCode("https://api.meetup.com/2/groups?sign=true&photo-host=public&country=au&key=37357c27353310f2d4f417f855721&city=Sydney","groups")
-        getResponseCode("https://api.meetup.com/find/upcoming_events?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721&lon=151.2100067138672&lat=-33.869998931884766","events")
-        '''
-        for item in mongo.db.cities.find()[0]["results"]:
-            getResponseCode("https://api.meetup.com/2/groups?sign=true&photo-host=public&country=au&key=37357c27353310f2d4f417f855721&lon="+str(item["lon"])+"&lat="+str(item["lat"]), "groups")
-        
-        for item in mongo.db.cities.find()[0]["results"]:
-            getResponseCode("https://api.meetup.com/find/upcoming_events?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721&lon="+str(item["lon"])+"&lat="+str(item["lat"]),"events")
-        '''
-        getResponseCode("https://api.meetup.com/topics?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721","topics")
+    storeToMongodb("https://api.meetup.com/topics?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721","topics")
 
 
-        for item in mongo.db.groups.find()[0]["results"]:
-            #for item_per_country in item["results"]:
-            getResponseCode("https://api.meetup.com/2/members?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721&group_id="+str(item["id"]),"members")
-        getResponseCode("https://api.meetup.com/find/venues?&sign=true&photo-host=public&key=37357c27353310f2d4f417f855721&text=Australia&location=Sydney", "venues")
-        '''
-        for item in mongo.db.cities.find()[0]["results"]:
-                getResponseCode("https://api.meetup.com/find/venues?&sign=true&photo-host=public&key=37357c27353310f2d4f417f855721&text=Australia&location="+item["city"],"venues")
-        '''
-        return "hhd"
+    for item in mongo.db.groups.find()[0]["results"]:
+        #for item_per_country in item["results"]:
+        storeToMongodb("https://api.meetup.com/2/members?sign=true&photo-host=public&key=37357c27353310f2d4f417f855721&group_id="+str(item["id"]),"members")
+    storeToMongodb("https://api.meetup.com/find/venues?&sign=true&photo-host=public&key=37357c27353310f2d4f417f855721&text=Australia&location=Sydney", "venues")
 
-#https://api.meetup.com/2/events?key=37357c27353310f2d4f417f855721&group_urlname=ny-tech&sign=true
-#https://api.meetup.com/2/categories?sign=true&photo-host=public&fields=2&page=20&key=37357c27353310f2d4f417f855721&group_urlname=ny-tech&sign=true
-def getResponseCode(url,collection_name):
+    return "hhd"
+
+#Store information about australia's bus stop using https://opendata.transport.nsw.gov.au/node/
+@app.route('/bus/',methods=['GET'])
+def storeBusInfo():
+
+    for item in mongo.db.venues.find():
+        #url="https://api.transport.nsw.gov.au/v1/tp/coord?outputFormat=rapidJSON&coord=151.209778%3A-33.871082%3AEPSG%3A4326&coordOutputFormat=EPSG%3A4326&inclFilter=1&type_1=BUS_POINT&radius_1=1000&PoisOnMapMacro=true&version=10.2.1.42"
+        url = "https://api.transport.nsw.gov.au/v1/tp/coord?outputFormat=rapidJSON&coord="+str(item["lon"])+"%3A"+str(item["lat"])+"%3AEPSG%3A4326&coordOutputFormat=EPSG%3A4326&inclFilter=1&type_1=BUS_POINT&radius_1=1000&PoisOnMapMacro=true&version=10.2.1.42"
+        #print(url)
+        #print(requests.get(url,headers={"Authorization":"apikey 5KkTkqJFDX4PARiheh73aiEEQ7LyrJxyURVV"}).json())
+        storeToMongodb(url,"busInfo")
+
+        #storeToMongodb("https://api.transport.nsw.gov.au/v1/tp/stop_finder?outputFormat=rapidJSON&type_sf=stop&coordOutputFormat=EPSG%3A4326&TfNSWSF=true&version=10.2.1.42&name_sf="+"%20".join(item["address_1"].split(" ")),"busInfo")
+
+
+    return "hhd"
+
+# store the data to mongodb
+def storeToMongodb(url,collection_name):
 
     try:
-        data = requests.get(url).json()
+        if collection_name=="busInfo":
+            data = requests.get(url,headers={"Authorization":"apikey 5KkTkqJFDX4PARiheh73aiEEQ7LyrJxyURVV"}).json()
+        else:
+            data = requests.get(url).json()
     except:
         return
 
@@ -54,32 +62,10 @@ def getResponseCode(url,collection_name):
             collection = mongo.db.create_collection(collection_name)
             if(data != []):
                 collection.insert(data)
-                #print(data)
-                if collection_name=="cities":
-                    f=csv.writer(open('./data/cities.csv', 'w'))
-
-                    f.writerow(["zip", "country", "localized_country_name", "distance", "city","lon","ranking","id","member_count","lat"])
-                    for x in mongo.db.cities.find()[0]["results"]:
-                        f.writerow([x["zip"],x["country"],x["localized_country_name"],x["distance"],x["city"],x["lon"],x["ranking"],x["id"],x["member_count"],x["lat"]])
-                    '''
-                    f.writerow(["zip", "country", "localized_country_name", "distance", "city","lon","ranking","id","member_count","lat"])
-                    for x in mongo.db.cities.find()[0]["results"]:
-                        f.writerow([x["zip"],x["country"],x["localized_country_name"],x["distance"],x["city"],x["lon"],x["ranking"],x["id"],x["member_count"],x["lat"]])
-                    '''
-
         else:
             if(data!=[]):
                 mongo.db[collection_name].insert(data)
-                #print(data)
-                if collection_name == "cities":
-                    f=csv.writer(open('./data/cities.csv', 'w'))
-                    for i,v in enumerate(data['results']):
-                        #print(i,v)
-                        key = v.keys()
-                        value = v.values()
-                        if i == 0:
-                            f.writerow(key)
-                        f.writerow(value)
+
 
 
     return 1
